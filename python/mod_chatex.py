259d998576dbd0ead021d1fb6c6cb9baeee83e81
chirimenmonster <chirimenmonster@users.noreply.github.com>
2020-09-12 10:02:09 +0900

    add feature: customize chat messages

--- python/mod_chatex.py
+++ python/mod_chatex.py
@@ -1,10 +1,16 @@
 
+import os
 import logging
+import json
+from functools import partial
 
-import game
+import ResMgr
 import Keys
+from Event import Event
+from PlayerEvents import g_playerEvents
 from debug_utils import LOG_CURRENT_EXCEPTION
 from helpers import getClientLanguage
+from gui import g_keyEventHandlers
 from messenger.proto.interfaces import IEntityFindCriteria
 from messenger.m_constants import BATTLE_CHANNEL, PROTO_TYPE
 from messenger import MessengerEntry
@@ -14,6 +20,10 @@ class MOD:
     PACKAGE_ID = '${package_id}'
     NAME = '${name}'
     VERSION = '${version}'
+    CONFIG_FILES = [
+        '${resource_file}',
+        '${config_file}'
+    ]
 
 _logger = logging.getLogger(MOD.NAME)
 
@@ -21,47 +31,83 @@ MESSAGE_DEF = {
     'en': {
         'ASSERT': 'Affirmative!',
         'NEGATE': 'Negative!'
-    },
-    'ja': {
-        'ASSERT': '了解! (Affirmative!)',
-        'NEGATE': '拒否! (Negative!)'
     }
 }
 
-class MESSAGE:
-    pass
+g_controller = None
 
-def overrideMethod(cls, method):
-    def decorator(handler):
-        orig = getattr(cls, method)
-        newm = lambda *args, **kwargs: handler(orig, *args, **kwargs)
-        if type(orig) is not property:
-            setattr(cls, method, newm)
-        else:
-            setattr(cls, method, property(newm))
-    return decorator
 
-@overrideMethod(game, 'handleKeyEvent')
-def _handleKeyEvent(orig, event):
-    ret = orig(event)
-    try:
+class KeyHandler(object):
+    def __init__(self):
+        self.__keyEvents = {}
+
+    def addKeyEvent(self, key, handler):
+        if key not in self.__keyEvents:
+            self.__keyEvents[key] = Event()
+        self.__keyEvents[key] += handler
+
+    def handleKeyEvent(self, event):
         if event.isKeyDown() and not event.isRepeatedEvent():
-            if event.key == Keys.KEY_F5:
-                sendTeamMessage(MESSAGE.ASSERT)
-            elif event.key == Keys.KEY_F6:
-                sendTeamMessage(MESSAGE.NEGATE)
-    except:
-        LOG_CURRENT_EXCEPTION()
-    return ret
+            handlers = self.__keyEvents.get(event.key, None)
+            if handlers is not None:
+                handlers()
+                return True
+        return False
+
+
+class Controller(object):
+    def __init__(self, config):
+        self.keyHandler = KeyHandler()
+        for name, value in config.items():
+            key = getattr(Keys, name)
+            self.keyHandler.addKeyEvent(key, partial(sendTeamMessage, value))
+        g_playerEvents.onAvatarBecomePlayer += self.__onAvatarBecomePlayer
+        g_playerEvents.onAvatarBecomeNonPlayer += self.__onAvatarBecomeNonPlayer
+
+    def __onAvatarBecomePlayer(self):
+        _logger.info('PlayerEvents: onAvatarBecomePlayer')
+        g_keyEventHandlers.add(self.keyHandler.handleKeyEvent)
+
+    def __onAvatarBecomeNonPlayer(self):
+        _logger.info('PlayerEvents: onAvatarBecomeNonPlayer')
+        g_keyEventHandlers.remove(self.keyHandler.handleKeyEvent)
+
 
 def init():
     _logger.info('initialize: %s %s', MOD.PACKAGE_ID, MOD.VERSION)
-    msglng = lng = getClientLanguage()
-    if msglng not in MESSAGE_DEF:
-        msglng = 'en'
-    _logger.info('client language = %s, chat message language = %s', lng, msglng)
-    for name, value in MESSAGE_DEF[msglng].items():
-        setattr(MESSAGE, name, value)
+    config = readConfig(MOD.CONFIG_FILES)
+    lang = getClientLanguage()
+    msglang = lang if lang in config else 'en'
+    _logger.info('client language = %s, chat message language = %s', lang, msglang)
+    global g_controller
+    g_controller = Controller(config[msglang])
+
+
+def readConfig(files):
+    def encode_value(data):
+        def encode_(x):
+            if isinstance(x, unicode):
+                x = x.encode('utf-8')
+            return x
+        return { k:encode_(v) for k, v in data.items() }
+    config = MESSAGE_DEF.copy()
+    _logger.info('cwd: %s', os.getcwd())
+    for file in files:
+        absfile = os.path.join('res', file)
+        absfile = os.path.abspath(absfile) if os.path.isfile(absfile) else file
+        if not ResMgr.isFile(file):
+            continue
+        _logger.info('loading config file: %s', absfile)
+        section = ResMgr.openSection(file)
+        try:
+            data = json.loads(section.asString, object_hook=encode_value)
+        except ValueError as e:
+            _logger.error(e)
+            continue
+        config.update(data)
+    #_logger.info('message settings:\n%s', json.dumps(config, ensure_ascii=False, indent=2))
+    return config
+
 
 # ex.
 #   scripts/client/messenger/proto/bw/find_criteria.py
